e09c5f992a26e47d3118140c221e25a878a18835
Victor Asuquo <veasuquo@gmail.com>
2023-11-04 17:00:50 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">My Shopping Cart</a>
-      </header>
-      <main>Product List</main>
-      <footer>All Rights Reserved</footer>
-    </div>
-  );
+import React from "react";
+import data from "./data.json";
+import Products from "./components/products";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">My Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All Rights Reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
